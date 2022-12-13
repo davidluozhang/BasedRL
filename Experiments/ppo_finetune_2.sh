2f7cdf9fc99fb6e01a411cebf5a5ec212e8ef7ef
David Zhi LuoZhang <51495738+davidluozhang@users.noreply.github.com>
2022-12-13 15:26:01 -0500

    in-progress debugging PPOMasked

---
+++ Experiments/ppo_finetune_2.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+echo "===== Debugging script for pretraining poacher agent in new environment ====="
+echo "===== ppo_finetune, poacher survival reward, ranger first, short epochs, fast alternation, starting ====="
+python ~/BasedRL/main.py --agent-id 1 --agent-learn "ppo" --epoch 1 --step-per-epoch 1000 --lr 1e-2 --agent-opponent "ppo" --resume-path "ppo_pretrained/ranger/gsg/ppo/policy.pth" --opponent-path "ppo_pretrained/poacher/gsg/ppo/policy.pth" --logdir "ppo_fix_4/ranger" --eps-train 0.15
+python ~/BasedRL/main.py --agent-id 0 --agent-learn "ppo" --epoch 1 --step-per-epoch 1000 --lr 1e-2 --agent-opponent "ppo" --resume-path "ppo_pretrained/poacher/gsg/ppo/policy.pth" --opponent-path "ppo_pretrained/ranger/gsg/ppo/policy.pth" --logdir "ppo_fix_4/poacher" --eps-train 0.15
+for i in {1..10}
+do
+echo "===== Iteration $i ====="
+python ~/BasedRL/main.py --agent-id 1 --agent-learn "ppo" --epoch 1 --step-per-epoch 1000 --lr 5e-2 --agent-opponent "ppo" --eps_clip .1 --recompute_advantage True --resume-path "ppo_fix_4/ranger/gsg/ppo/policy.pth" --opponent-path "ppo_fix_4/poacher/gsg/ppo/policy.pth" --logdir "ppo_fix_4/ranger" --iter $i --eps-train 0.15
+echo "===== Training Poacher Now ====="
+python ~/BasedRL/main.py --agent-id 0 --agent-learn "ppo" --epoch 1 --step-per-epoch 1000 --lr 5e-2 --agent-opponent "ppo" --eps_clip .1 --recompute_advantage True --resume-path "ppo_fix_4/poacher/gsg/ppo/policy.pth" --opponent-path "ppo_fix_4/ranger/gsg/ppo/policy.pth" --logdir "ppo_fix_4/poacher" --iter $i --eps-train 0.15
+done
+echo "===== ppo_finetune, poacher survival reward, ranger first, short epochs, fast alternation, finished ====="
